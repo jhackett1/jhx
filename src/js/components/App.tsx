14e9d1c834c2de39772af9725a58143d449f0349
Jaye Hackett <jaye.hackett@gmail.com>
2021-09-05 18:10:55 +0100

    style up some dialogs

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -1,5 +1,17 @@
 import * as React from "react"
+import Dialog from "./Dialog"
 
-const App = () => <h1>Testing</h1>
+const App = () => {
+  const path = window.location.pathname
+
+  return (
+    <div style={{ height: "400px" }}>
+      testing ghfdksl ghfjd ghfjd ghfdjk
+      <Dialog>Testing</Dialog>testing
+    </div>
+  )
+
+  return null
+}
 
 export default App
--- src/js/components/Dialog.tsx
+++ src/js/components/Dialog.tsx
@@ -1,6 +1,53 @@
-// import React from "react"
-// import { Dialog as RawDialog } from "@reach/dialog"
+import * as React from "react"
+import { Dialog as ReachDialog } from "@reach/dialog"
 
-// const Dialog = ({ children }) => <RawDialog>{children}</RawDialog>
+interface Props {
+  title: string
+  children: React.ReactChildren
+}
 
-// export default Dialog
+const Dialog = ({ title, children }: Props) => {
+  const handleClose = () => (window.location.pathname = "/")
+
+  return (
+    <ReachDialog
+      aria-label={title}
+      className="dialog"
+      isOpen={true}
+      onDismiss={handleClose}
+    >
+      <header className="dialog__header">
+        <button className="dialog__close" onClick={handleClose}>
+          <span className="visually-hidden">Close</span>
+          <svg
+            width="30"
+            height="29"
+            viewBox="0 0 30 29"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="1.15668"
+              y="26.3298"
+              width="36.9165"
+              height="2.80379"
+              transform="rotate(-45 1.15668 26.3298)"
+            />
+            <rect
+              x="26.9301"
+              y="28.3124"
+              width="36.9165"
+              height="2.80379"
+              transform="rotate(-135 26.9301 28.3124)"
+            />
+          </svg>
+        </button>
+
+        <h1>{title}</h1>
+      </header>
+
+      {children}
+    </ReachDialog>
+  )
+}
+
+export default Dialog
