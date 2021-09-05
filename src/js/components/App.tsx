e78de00bee78af5426b05fcdc6e4246dffbbcfc7
Jaye Hackett <jaye.hackett@gmail.com>
2021-09-05 17:33:24 +0100

    add react

---
+++ src/js/components/App.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+
+const App = () => <h1>Testing</h1>
+
+export default App
---
+++ src/js/components/Dialog.tsx
@@ -0,0 +1,6 @@
+// import React from "react"
+// import { Dialog as RawDialog } from "@reach/dialog"
+
+// const Dialog = ({ children }) => <RawDialog>{children}</RawDialog>
+
+// export default Dialog
